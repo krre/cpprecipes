0b642f0d835ae78e86021463ff10654faa689498
Vladimir Zarypov <krre31@gmail.com>
2023-08-28 20:59:43 +0300

    Add consts recipe

---
+++ consts/main.cpp
@@ -0,0 +1,84 @@
+int main() {
+    {
+        // Equal expressions. Const value.
+        const int x = 5;
+        int const y = 10;
+
+        // Compile error.
+        // x = 0;
+        // y = 0;
+
+        // int* px = &x;
+        // int* py = &y;
+    }
+
+    {
+        int x = 5;
+        int y = 10;
+
+        // Equal expressions. Non-const pointer on const value.
+        const int* px = &x;
+        int const* py = &y;
+
+        // Compile error.
+        // *px = 0;
+        // *py = 0;
+
+        px = nullptr;
+        py = nullptr;
+    }
+
+    {
+        int x = 5;
+
+        // Const pointer on non-const value.
+        int* const px = &x;
+
+        *px = 0;
+
+        // Compile error.
+        // px = nullptr;
+    }
+
+    {
+        int x = 5;
+        int y = 10;
+
+        // Equal expressions. Const pointer on const value.
+        const int* const px = &x;
+        int const* const py = &y;
+
+        // Compile error.
+        // px = nullptr;
+        // py = nullptr;
+
+        // *px = 0;
+        // *py = 0;
+    }
+
+    {
+        int x = 5;
+
+        // Reference on non-const value.
+        int& rx = x;
+        int& ry = rx;
+
+        rx = 0;
+        ry = 1;
+    }
+
+    {
+        int x = 5;
+        int y = 10;
+
+        // Equal expressions. Reference on const value.
+        int const& rx = x;
+        const int& ry = y;
+
+        // Compile error.
+        // rx = ry;
+        // rx = 0;
+    }
+
+    return 0;
+}
