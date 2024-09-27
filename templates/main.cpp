0206f4fb2aefb3135302a84430d5c2bac74cf2d8
Vladimir Zarypov <krre31@gmail.com>
2024-09-27 05:33:55 +0300

    Remove 'using namespace std'

--- templates/main.cpp
+++ templates/main.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 
-using namespace std;
-
 template <typename T>
 auto sum(T a, T b) {
     return a + b;
 }
 
 int main() {
-    cout << sum(2, 3) << endl;
+    std::cout << sum(2, 3) << std::endl;
 
     return 0;
 }
